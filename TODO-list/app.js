795e0302a5b55fe54a91bb38ec6cc8ff061b8063
Sampath <51907206+sampath88@users.noreply.github.com>
2025-04-19 19:28:17 +0530

    Update app.js

--- TODO-list/app.js
+++ TODO-list/app.js
@@ -1,4 +1,4 @@
-//selectors
+//selectors update
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
